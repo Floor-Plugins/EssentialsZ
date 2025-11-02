583c87f20beb40ae2596361871c45ab552f0d125
boyninja15 <flappyfloorg@gmail.com>
2025-11-01 22:38:28 -0500

    Basic Paper plugin

---
+++ src/main/java/com/floorplugins/essentialsz/EssentialsZ.java
@@ -0,0 +1,10 @@
+package com.floorplugins.essentialsz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EssentialsZ extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
