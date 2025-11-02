eaedd045e7999c22d6b5252a9fb942c239f00731
boyninja15 <flappyfloorg@gmail.com>
2025-11-02 09:55:26 -0600

    Added configuration reloaded message

--- src/main/java/com/floorplugins/essentialsz/commands/ReloadCommand.java
+++ src/main/java/com/floorplugins/essentialsz/commands/ReloadCommand.java
@@ -1,24 +1,37 @@
 package com.floorplugins.essentialsz.commands;
 
+import com.floorplugins.essentialsz.utils.MessageUtils;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
+
 public class ReloadCommand implements BasicCommand {
     public final JavaPlugin plugin;
+    public final MessageUtils messageUtils;
+    public final MiniMessage mm = MiniMessage.miniMessage();
 
     public ReloadCommand(JavaPlugin plugin) {
         this.plugin = plugin;
+        this.messageUtils = new MessageUtils(this.plugin);
     }
 
     @Override
     public void execute(@NotNull CommandSourceStack source, String @NotNull [] args) {
         plugin.reloadConfig();
+        send(source.getSender(), "success", "config-reloaded", null);
     }
 
     @Override
     public String permission() {
         return "essentialsz.admin";
+    }
+
+    private void send(CommandSender sender, String type, String path, Map<String, String> replace) {
+        sender.sendMessage(mm.deserialize(messageUtils.withFormat(type, messageUtils.get("config", path, replace))));
     }
 }
